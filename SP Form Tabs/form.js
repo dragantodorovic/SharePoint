7a0fa818f6486815ae87d1ac563b98727cf9ce37
Dragan Todorovic <dragan.todorovic@outlook.com>
2017-05-08 15:17:59 +0200

    Create form.js

---
+++ SP Form Tabs/form.js
@@ -0,0 +1,24 @@
+<script src="//code.jquery.com/jquery-1.10.1.min.js"></script>
+<style type="text/css">
+  .ms-formtable
+  {display:none;}
+</style>
+<script type="text/javascript">
+    $(document).ready(function() {
+        //loop      
+        $("span.gagiForm").each(function()
+        {
+            //get the display name
+            displayName = $(this).attr("data-displayName");
+
+            displayName  = displayName.replace(/&(?!amp;)/g,'&amp;');
+            elem = $(this);
+            //find the corresponding field 
+            $("table.ms-formtable td").each(function(){
+                if (this.innerHTML.indexOf('FieldName="'+displayName+'"') != -1){
+                    $(this).contents().appendTo(elem);
+                }
+            });
+        });
+    });
+</script>
